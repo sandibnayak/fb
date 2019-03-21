268183e37e72feeb16cdc97fedd4a0452cbaf060
sandibnayak <sandibnayak@gmail.com>
2019-03-21 12:40:24 +0530

    2sn fb login

--- src/main/java/com/fb/StepDefination/FBLogin.java
+++ src/main/java/com/fb/StepDefination/FBLogin.java
@@ -1,39 +1,46 @@
 package com.fb.StepDefination;
 
-import java.util.List;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import cucumber.api.java.en.*;
-import io.cucumber.datatable.DataTable;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 
 public class FBLogin {
+	static WebDriver driver;
+
 	@Given("User should in Login Home Page")
 	public void user_should_in_Login_Home_Page() {
 		System.out.println("Given Block");
+		System.setProperty("wedriver.chrome.driver",
+				"C:\\Users\\SANDIB KUMAR NAYAK\\eclipse-workspace\\CucumberClass\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
 	}
 
-	@When("User have pass the user ID <user_id> and password<pass>")
-	public void user_have_pass_the_user_ID_user_id_and_password_pass(DataTable dataTable) {
-		// Write code here that turns the phrase above into concrete actions
-		// For automatic transformation, change DataTable to one of
-		// E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-		// Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-		// Double, Byte, Short, Long, BigInteger or BigDecimal.
-		//
-		// For other transformations you can register a DataTableType.
-		List<String> d = dataTable.asList();
+	@When("User have pass the user id and password")
+	public void user_have_pass_the_user_ID_user_id_and_password_pass() {
 		System.out.println("When Block");
-		System.out.println(dataTable);
-		System.out.println("1st data" + d.get(0) + "2nd Data" + d.get(1));
+		driver.findElement(By.name("email")).sendKeys("abcd");
+		driver.findElement(By.name("pass")).sendKeys("pass");
+
 	}
 
 	@When("User need to click the login button")
 	public void user_need_to_click_the_login_button() {
 		System.out.println("And Block");
+		driver.findElement(By.xpath("//input[@value=\"Log In\"]")).click();
 	}
 
 	@Then("Need to check user in fb Home Page")
 	public void need_to_check_user_in_fb_Home_Page() {
 		System.out.println("Then Block");
+		String rt = driver.findElement(By.xpath("//span[text()=\"Log in to Facebook\"]")).getText();
+	Assert.assertEquals("Log in to Facebook", rt);
 	}
 
 }
--- target/test-classes/FeatureFiles/FbLogIn.feature
+++ target/test-classes/FeatureFiles/FbLogIn.feature
@@ -2,7 +2,6 @@ Feature: Title of your feature
 
   Scenario: Title of your scenario
     Given User should in Login Home Page
-    When User have pass the user ID <user_id> and password<pass>
-    |user_id1|pass1|
+    When User have pass the user id and password
     And User need to click the login button
     Then Need to check user in fb Home Page
